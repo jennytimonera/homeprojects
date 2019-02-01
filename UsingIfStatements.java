65c7d27686a3ee14ccb96ad9a220c305a7da53c3
Jenny <jennytimonera@yahoo.com>
2019-02-01 06:18:01 -0500

    Second try to upload in Git Hub.

---
+++ UsingIfStatements.java
@@ -0,0 +1,18 @@
+public class UsingIfStatements {
+	public static void main(String[] args) {
+
+		int x = 10;
+		int y = 20;
+		
+		if (x < y) {
+			System.out.println("x is less than y");
+		}
+
+		if (x > y) {
+			System.out.println("x is greater than y");
+		}
+		if (x == y) {
+			System.out.println("x is equals to y");
+		}
+	}
+}
